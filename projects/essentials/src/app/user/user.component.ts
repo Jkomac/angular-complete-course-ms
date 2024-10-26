e208640c183a8a34f8eb709eb995b4f4a0607121
Jose <jkomac19@gmail.com>
2024-10-26 17:50:57 +0200

    [SIGNALS METHOD] -- New method to update the selectedUser information from Angular 16 onwards

--- projects/essentials/src/app/user/user.component.ts
+++ projects/essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core'; // Signals and Computed added
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); // Generating a random index to select a random user from the list of dummy users || Math.random() = [0.0, 1.0) || Math.floor() rounds down to the nearest integer
@@ -9,16 +9,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); // Generatin
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
 
-  // Getter to dynamically return the path of the image of the selected user
-  get imagePath() {
-   return 'assets/users/' + this.selectedUser.avatar;
-  }
-  
-  onSelectUser() {
-   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-   this.selectedUser = DUMMY_USERS[randomIndex];
-  }
- }
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // Signal to hold the selected user and automatically trigger change detection when updated || Selecting a random user from the list of dummy users
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // Computed value to dynamically return the path of the image of the selected user (similar to a Getter but reactive) || Signals must be called as functions.
+
+ // Method to be called when a user is selected
+ onSelectUser() {
+  const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  this.selectedUser.set(DUMMY_USERS[randomIndex]);
+ }  
+}
