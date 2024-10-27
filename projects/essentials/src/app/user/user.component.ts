f63b9227c5651b0eb9c595d7ec81ba622462b4d5
Jose <jkomac19@gmail.com>
2024-10-27 20:43:41 +0100

    Formatting & Ensuring EventEmitter is of type string

--- projects/essentials/src/app/user/user.component.ts
+++ projects/essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'; // Note: 'Input' starts with an uppercase 'I'
+import { Component, EventEmitter, Input, Output } from '@angular/core'; // Note: 'Input' and 'Output' start with an uppercase 'I' and 'O'
 
 @Component({
   selector: 'app-user',
@@ -9,12 +9,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'; // Note:
 
 export class UserComponent {
   // Input decorator to allow the parent component to pass data to this component
-  @Input({ required: true}) id!: string; // Marked as required to ensure the parent component provides a value || Non-null assertion operator (!) is used to tell TypeScript that the variable will be initialized later
+  @Input({ required: true }) id!: string; // Marked as required to ensure the parent component provides a value || Non-null assertion operator (!) is used to tell TypeScript that the variable will be initialized later
   @Input({ required: true }) avatar!: string; 
   @Input({ required: true }) name!: string;
 
   // Output decorator to allow this component to pass data to the parent component through events
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>(); // Angle brackets are used as an extra layer of security (optionals) to ensure that the EventEmitter is of type string
 
   // Getter to dynamically return the path of the image of the users
   get imagePath() {
