d0d20e73c5898dc60dad5e81a3e686cc291bbbd4
Jose <jkomac19@gmail.com>
2024-10-29 20:55:07 +0100

    Structural Directives *ngFor and *ngIf added (Traditional method)

--- projects/essentials/src/app/app.component.ts
+++ projects/essentials/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root', // Selector to use the component in HTML file
   standalone: true, // Indicates whether the component is independent (true) or needs to be declared in an Angular module (false)
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent], // Importing those components which are used in this component
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf], // Importing those components which are used in this component
   templateUrl: './app.component.html', // Path to the HTML file to be rendered || HTML code can be written here (inline template) by using `template` instead of `templateUrl`
   styleUrl: './app.component.css' // Path to the CSS file to be applied || CSS code can be written here (inline style) by using `styles` instead of `styleUrl`
 })
