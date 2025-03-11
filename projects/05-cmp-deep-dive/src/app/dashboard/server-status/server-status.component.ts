6713a5199b8051019c3ad6db148338a9c3e7eec1
Jose <jkomac19@gmail.com>
2025-03-11 21:02:16 +0100

    Enhance ServerStatus with Literal Types and Dynamic Status updates

--- projects/05-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ projects/05-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -9,5 +9,19 @@ import { Component } from '@angular/core';
 })
 export class ServerStatusComponent {
 
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline'; // Literal Types: Restricts the variable to accept only the specified literal values, providing better type safety and autocompletion ('online', 'offline', or 'unknown')
+
+  constructor(){
+    setInterval(() => { // setInterval: Executes the provided function repeatedly at fixed time intervals (5000 ms, 5s in this case)
+      const random = Math.random(); // [0-1)
+
+      if (random < 0.5){
+        this.currentStatus = 'online';
+      } else if(random < 0.9){
+        this.currentStatus = 'offline'
+      } else {
+        this.currentStatus = 'unknown'
+      }
+    }, 5000); 
+  }
 }
