733cb6df2677602e5175251369a7a63daa555fa6
Jose <jkomac19@gmail.com>
2025-03-14 12:42:43 +0100

    DestroyRef Cleanup - Modern Alternative

--- projects/05-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ projects/05-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, AfterViewInit { // Implements lifecycle hooks to ensure them to be called correctly (against any kind of typo) as part of the component's lifecycle
 
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline'; // Literal Types: Restricts the variable to accept only the specified literal values, providing better type safety and autocompletion ('online', 'offline', or 'unknown')
+  private destroyRef = inject(DestroyRef); // Injects the DestroyRef service to register a callback for when the component is destroyed
 
   ngOnInit(){ // Lifecycle hook that is called once after Angular has initialized ALL the component's inputs.
-    setInterval(() => { // setInterval: Executes the provided function repeatedly at fixed time intervals (5000 ms, 5s in this case)
+    console.log('ON INIT');
+    const interval = setInterval(() => { // setInterval: Executes the provided function repeatedly at fixed time intervals (5000 ms, 5s in this case)
       const random = Math.random(); // [0-1)
 
       if (random < 0.5){
@@ -22,6 +24,15 @@ export class ServerStatusComponent {
       } else {
         this.currentStatus = 'unknown'
       }
-    }, 5000); 
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => { // Registers a callback to be executed when the component is destroyed
+      clearInterval(interval); // Callback: A function that is passed as an argument to another function and is executed after some event or condition occurs
+    })
   }
+
+  ngAfterViewInit(){ // Lifecycle hook that is called once after the component's view (component's template) has been initialized.
+    console.log('AFTER VIEW INIT');
+  }
+
 }
