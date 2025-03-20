9f6580c13b1a62787588fcdf85f85d0f5d4b2057
Jose <jkomac19@gmail.com>
2025-03-20 10:16:46 +0100

    ViewChild Decorator - Traditional way

--- projects/05-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ projects/05-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -13,11 +13,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>; // @ViewChild: Allow us to access to an element or child component of its own template || @ViewChild('nameTemplateVariableWithout#') variableName: ElementRef<typeOfTemplateVariable>
+
   onSubmit(titleElement: HTMLInputElement, textInput: HTMLTextAreaElement){ // If the template variable were assigned to some other element, the type would reflect that element (e.g. Label -> HTMLLabelElement)
     // console.dir(titleElement); // Shows the properties of titleElement object to the console in a more detailed and interactive way
     const enteredTitle = titleElement.value;
     const enteredText = textInput.value;
     console.log('Entered value: ' + enteredTitle);
     console.log('Entered text: ' + enteredText);
+    this.form?.nativeElement.reset(); // Resets the form, clearing all the input elements inside of it
   }
 }
