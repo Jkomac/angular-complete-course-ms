d6f3c98fe81f84fd0e16903d23bfcb408144ad54
Jose <jkomac19@gmail.com>
2025-03-20 10:33:59 +0100

    viewChild Signal - Modern way

--- projects/05-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ projects/05-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -13,11 +13,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); // ViewChild Signal: Allow us to access to an element/child component of its own template || variableName = viewChild<ElementRef<typeOfTemplateVariable>>('nameTemplateVariableWithout#')
+
   onSubmit(titleElement: HTMLInputElement, textInput: HTMLTextAreaElement){ // If the template variable were assigned to some other element, the type would reflect that element (e.g. Label -> HTMLLabelElement)
     // console.dir(titleElement); // Shows the properties of titleElement object to the console in a more detailed and interactive way
     const enteredTitle = titleElement.value;
     const enteredText = textInput.value;
     console.log('Entered value: ' + enteredTitle);
     console.log('Entered text: ' + enteredText);
+    this.form().nativeElement.reset(); // ElementRef is a wrapper around a native DOM element. Use the nativeElement property to access the actual DOM element.
   }
 }
